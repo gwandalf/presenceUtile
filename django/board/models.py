c0299900d9405ae7baa29cca55fc7fe7689d16b7
gwandalf <gwlemoul@insa-rennes.fr>
2015-01-16 23:33:02 +0100

    Mission

--- django/board/models.py
+++ django/board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Mission(models.Model):
+
+    title = models.TextField(null=True)
+    content = models.TextField(null=True)
+    top_point_x = models.IntegerField()
+    top_point_y = models.IntegerField()
+
+    def take(self):
+        pass
